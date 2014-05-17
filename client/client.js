a5011fa5e5df640420393e760f735330cf7e0f4e
Sam Corcos <sam@andaril.com>
2014-05-16 19:23:42 -0700

    finished home page

--- client/client.js
+++ client/client.js
@@ -1,45 +1,96 @@
-attackSuccess = function(attack, defense) {
-    sum = attack + defense
-    frac = attack / sum
-    randomSample = Math.random()
-    return randomSample < frac // attack wins if true
-}
-
 computeBattle = function(battle) {
     // console.log(battle)
     // attacking/defending, supplied/unsupplied
 
     // total defensive strength, total attacking strength, (TODO: modifiers), call attackSuccess
 
-    defensiveStrength = armyValue(battle.defending, 'defense')
-    attackingStrength = armyValue(battle.attacking, 'attack')
+    // console.log('battle begins')
+    // console.log(battle)
 
-    success = attackSuccess(attackingStrength, defensiveStrength)
+    data = {}
 
-    console.log(success)
+    data.winner = computeWin(battle)
+    _.extend(data, computeCasualties(battle))
 
-    // return false to prevent POST request
-    return false
+    console.log(data)
+    return data
 }
-
 
 AutoForm.hooks({
     battle: {
-        onSubmit: computeBattle
+        onSubmit: function(battle) {
+            try {
+                data = computeBattle(battle)
+                Session.set("battleOutcomeData", data)
+                Router.go('battleOutcome')
+            } catch (err) {
+                console.log(err.stack)
+            }
+            // return false to prevent POST request
+            return false;
+        }
     }
 });
 
+// Template.battleForm.events({
+//     'click div.fight': function(event, template) {
 
-armyValue = function(army, param) { // param = 'defense' or 'cost' or 'attack'
-    accumulator = 0
-    suppliedAndUnsupplied = _.keys(army) // ['supplied', 'unsupplied']
-    suppliedAndUnsupplied.forEach(function(suppliedOrUnsupplied) {
-        unitNames = _.keys(army[suppliedOrUnsupplied]) // ['infantry', 'armor', ...]
-        unitNames.forEach(function(unitName) {
-            numberOfUnits = army[suppliedOrUnsupplied][unitName]
-            unitStrength = unitStrengths[suppliedOrUnsupplied][unitName][param]
-            accumulator += numberOfUnits * unitStrength
-        })
-    })
-    return accumulator
+//     }
+// })
+
+computeWin = function(battle) {
+    defensiveStrength = armyValue(battle.defending, 'defense')
+    attackingStrength = armyValue(battle.attacking, 'attack')
+    // console.log('defensive strength:', defensiveStrength)
+    // console.log('attacking strength:', attackingStrength)
+
+    success = attackSuccess(attackingStrength, defensiveStrength)
+    // success ? console.log("the attack succeeded") : console.log("the defense succeeded")
+    return success ? "ATTACK WINS" : "DEFENSE WINS"
+}
+
+computeCasualties = function(battle) {
+    // each unit does a 50% roll to possibly kill twice (after everyone has attacked once. if this unit dies, he kill again)
+    // remembers that offense and defense have different kill scores (out of 6)
+    // sum up kills for each army
+
+    // for each army
+    // for each unit
+    // random roll based on defense or attack value out of 6
+    // sum the kills
+    // 
+    // then randomly select and remove units from each army based on kills
+    // 
+    // each army
+    // each unit
+    // 50% roll to attack again
+    // everything again. - recursive.
+
+    defendingArmyList = armyToList(battle.defending)
+    attackingArmyList = armyToList(battle.attacking)
+    // console.log("initial defensive army:", defendingArmyList)
+    // console.log("initial attacking army:", attackingArmyList)
+
+    defensiveKills = armyKills(battle.defending, 'defense')
+    attackingKills = armyKills(battle.attacking, 'attack')
+
+    defendingCasualties = _.sample(defendingArmyList, attackingKills)
+    attackingCasualties = _.sample(attackingArmyList, defensiveKills)
+    // console.log("defensive kills:", defensiveKills)
+    // console.log("defensive casualties:", defendingCasualties)
+    // console.log("attacking kills:", attackingKills)
+    // console.log("attacking casualties:", attackingCasualties)
+
+    removeOneEachFromList(defendingArmyList, defendingCasualties)
+    removeOneEachFromList(attackingArmyList, attackingCasualties)
+
+    // console.log("remaining defensive army:", defendingArmyList)
+    // console.log("remaining attacking army:", attackingArmyList)
+
+    return {
+        casualties: {
+            defending: mergeSuppliedAndUnsupplied(listToArmy(defendingCasualties)),
+            attacking: mergeSuppliedAndUnsupplied(listToArmy(attackingCasualties))
+        }
+    }
 }
